32591a895e2ea63b949ffb421104f1842176db67
Cameron Yuen <yuen.cameron@gmail.com>
2025-09-17 22:13:58 -0700

    wip

--- .eleventy.js
+++ .eleventy.js
@@ -1,37 +1,3 @@
-module.exports = function (eleventyConfig) {
-  // Copy static assets
-  eleventyConfig.addPassthroughCopy("assets");
+export default function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("admin");
-  eleventyConfig.addPassthroughCopy("_redirects");
-
-  // Add filters
-  eleventyConfig.addFilter("readableDate", (dateObj) => {
-    return new Date(dateObj).toLocaleDateString("en-US", {
-      year: "numeric",
-      month: "long",
-      day: "numeric",
-    });
-  });
-
-  eleventyConfig.addFilter("htmlDateString", (dateObj) => {
-    return new Date(dateObj).toISOString().split("T")[0];
-  });
-
-  // Collections
-  eleventyConfig.addCollection("posts", function (collectionApi) {
-    return collectionApi.getFilteredByGlob("posts/*.md").sort(function (a, b) {
-      return new Date(b.date) - new Date(a.date);
-    });
-  });
-
-  return {
-    dir: {
-      input: ".",
-      includes: "_includes",
-      layouts: "_layouts",
-      output: "_site",
-    },
-    markdownTemplateEngine: "njk",
-    htmlTemplateEngine: "njk",
-  };
-};
+}
